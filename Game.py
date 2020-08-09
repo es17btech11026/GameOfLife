f9d6a29f9422fc9e194887b181e80381fa605eb1
Adil Tanveer <es17btech11026@iith.ac.in>
2020-08-09 18:04:07 +0530

    Final Version

--- Game.py
+++ Game.py
@@ -2,7 +2,7 @@
 # AUTHOR		: ADIL TANVEER
 # LANGUAGE		: PYTHON3
 # DESCRIPTION	: This module simulates the Game of Life
-#				  For details of the game refer https://en.wikipedia.org/wiki/Conway%27s_Game_of_Lif
+# For details of the game refer https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
 # MODULE_DESCRIPTION("My kernel module - mykmod");
 # MODULE_AUTHOR("es17btech11026 [at] gmail.com");
@@ -27,8 +27,7 @@ def about():
 	percentSign = "%"
 	print("DESCRIPTION :")
 	print("1. This module simulates the Game of Life")
-	print("2. For details of the rules of the game refer https://en.wikipedia.org/wiki/Conway" \
-				+ percentSign + "27s_Game_of_Life#Rules")
+	print("2. For details of the rules of the game refer https://en.wikipedia.org/wiki/Conway" + percentSign + "27s_Game_of_Life#Rules")
 	print("3. The initial state of the universe is either random or has to be given as an input in a text file (only)")
 	print("4. The input file should only contain a 2D matrix of space separated integers with rows being separated by a newline")
 	print("5. For knowing the details of execution and operation of the module type the command:\n\tpython3 <filename> --usage")
@@ -77,7 +76,6 @@ def usageManual(height, width, fps):
 	print("Step 5: Click the cross icon to close the window and exit the module")
 
 def readInput():
-	
 	global height
 	global width
 	global fps
@@ -135,11 +133,8 @@ def readInput():
 	return inputUniverse	
 
 class Game:
-	
 	# INIT FUNCTION
-
 	def __init__(self, inputRoot, inputUniverse, inputFps):
-		
 		
 		#  Setup required for built-in logic
 		self.pause = True		# For pausing the animation whenever required
