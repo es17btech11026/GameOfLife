575d121596eaa57fc6fe1743424e9f53c810454e
ADIL TANVEER <47566766+es17btech11026@users.noreply.github.com>
2020-08-09 17:45:30 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,8 +1,8 @@
 ##########################################
 # AUTHOR		: ADIL TANVEER
 # LANGUAGE		: PYTHON3
-# DESCRIPTION	: This module simulates the Game of Life
-#				  For details of the game refer https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules
+# DESCRIPTION		: This module simulates the Game of Life
+#			  For details of the game refer https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules
 
 # MODULE_DESCRIPTION("My kernel module - mykmod");
 # MODULE_AUTHOR("es17btech11026 [at] gmail.com");
